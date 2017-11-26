edc349f67695420b39db3b17aa3e1121cb8a822f
Richard Tingstad <richard.tingstad@gmail.com>
2017-11-26 21:24:35 +0100

    make.sh generalized some to easier extend

--- make.sh
+++ make.sh
@@ -2,17 +2,25 @@
 #playing with java 9 modules, building manually
 set -o errexit
 
-[ -d mods ] && rm -r mods/
-mkdir mods
+main() {
+    [ -d mods ] && rm -r mods/
+    mkdir mods
 
-[ -d service-api/target ] && rm -r service-api/target/
-javac -d service-api/target/classes/ $(find service-api/src/main/java/ -name \*.java)
-jar --create --file mods/weather-service-api-1.0-SNAPSHOT.jar --module-version=1.0 -C service-api/target/classes/ .
-jar --file mods/weather-service-api-1.0-SNAPSHOT.jar --describe-module
+    for module in service-api ;do
+        build $module
+    done
+    build app com.github.tingstad.weather.app.Main
+    echo 'java --module-path mods/ --module com.github.tingstad.weather.app 8080'
+}
 
-[ -d app/target ] && rm -r app/target/
-javac --module-path mods/ -d app/target/classes/ $(find app/src/main/java/ -name \*.java)
-jar --create --file mods/weather-app-1.0-SNAPSHOT.jar --module-version=1.0 --main-class=com.github.tingstad.weather.app.Main -C app/target/classes/ .
-jar --file mods/weather-app-1.0-SNAPSHOT.jar --describe-module
+build() {
+    name="$1"
+    main="$([ -n "$2" ] && echo "--main-class=$2 " || echo "")"
+    [ -d $name/target ] && rm -r $name/target/
+    javac --module-path mods/ -d $name/target/classes/ $(find $name/src/main/java/ -name \*.java)
+    jar --create --file mods/weather-$name-1.0-SNAPSHOT.jar --module-version=1.0 ${main}-C $name/target/classes/ .
+    jar --file mods/weather-$name-1.0-SNAPSHOT.jar --describe-module
+}
 
-echo 'java --module-path mods/ --module com.github.tingstad.weather.app 8080'
+main
+
